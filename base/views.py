392bd57fabc938892c65c85c58e1e89a024899de
Aneeb <aneebpaloth74@gmail.com>
2023-07-29 10:42:35 -0700

    added database model

--- base/views.py
+++ base/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
+# rooms = [
+#     {"id":1, "name": "lets learn Python"},
+#     {"id":2, "name": "lets learn Java"},
+#     {"id":3, "name": "lets learn C++"}
+
+# ]
+
+
+def home(request):
+    rooms = Room.objects.all()
+    return render(request , 'base/home.html',{'rooms':rooms})
+
+
+def room(request,pk):
+    rooms = Room.objects.get(id=pk)
+    return render(request , 'base/room.html',{'rooms' : rooms})
