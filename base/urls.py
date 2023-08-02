6f196f2969e20d2476370de8079633a62b1e61e1
Aneeb <aneebpaloth74@gmail.com>
2023-08-02 10:40:17 -0700

    login funtion added

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name="home"),
+    path('login/',views.LoginPage,name='login'),
     path('room/<str:pk>',views.room,name="room"),
     path('create_room/',views.createRoom,name="create-room"),
     path('update_room/<str:pk>',views.updateRoom,name="update-room"),
--- base/views.py
+++ base/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 from .models import Room,Topic
 from django.db.models import Q
 from .form import RoomForm
@@ -10,6 +13,27 @@ from .form import RoomForm
 #     {"id":3, "name": "lets learn C++"}
 
 # ]
+
+def LoginPage(request):
+    if request.method == 'POST':
+        username= request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username) 
+        except:
+            messages.error(request, "username does not exist")
+
+        user = authenticate(request, username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request, "username name or password does not exist")
+
+
+    context= {}
+    return render(request,'base/login_register.html',context)
 
 
 def home(request):
